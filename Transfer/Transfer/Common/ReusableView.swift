50f4fc4570975a1b8e62526762612564d7aa8b56
Fateme <mohammadi.fatemeh071@gmail.com>
2023-05-30 18:28:58 +0330

    added ReusableView for header identifier

---
+++ Transfer/Transfer/Common/ReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ReusableView.swift
+//  Transfer
+//
+//  Created by Fateme on 2023-05-26.
+//
+
+import Foundation
+
+protocol ReusableView: AnyObject {
+    static var identifier: String { get }
+}
